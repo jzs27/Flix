3a350357161bf96312b7300fada22e71fa131dd3
jzs27 <jzs27@cornell.edu>
2021-06-23 16:47:04 -0500

    segue to details II

--- Flix/View Controllers/DetailsViewController.m
+++ Flix/View Controllers/DetailsViewController.m
@@ -36,6 +36,9 @@
     
     self.titleLabel.text = self.movie[@"title"];
     self.synopsisLabel.text = self.movie[@"overview"];
+    
+    [self.titleLabel sizeToFit];
+    [self.synopsisLabel sizeToFit];
     // Do any additional setup after loading the view.
 }
 
