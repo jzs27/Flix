5be80c68426ebfa3a06ee8d59432a34ff0658748
jzs27 <jzs27@cornell.edu>
2021-06-24 18:29:49 -0500

    large poster view

---
+++ Flix/View Controllers/PosterViewController.h
@@ -0,0 +1,18 @@
+//
+//  PosterViewController.h
+//  Flix
+//
+//  Created by jessicasyl on 6/24/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PosterViewController : UIViewController
+
+@property (nonatomic,strong) NSDictionary *movie;
+
+@end
+
+NS_ASSUME_NONNULL_END
--- Flix/View Controllers/PosterViewController.m
+++ Flix/View Controllers/PosterViewController.m
@@ -1,23 +1,20 @@
 //
-//  DetailsViewController.m
+//  PosterViewController.m
 //  Flix
 //
-//  Created by jessicasyl on 6/23/21.
+//  Created by jessicasyl on 6/24/21.
 //
 
-#import "DetailsViewController.h"
+#import "PosterViewController.h"
 #import "UIImageView+AFNetworking.h"
 
-@interface DetailsViewController ()
-@property (weak, nonatomic) IBOutlet UIImageView *backdropView;
+@interface PosterViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *posterView;
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
-@property (weak, nonatomic) IBOutlet UILabel *synopsisLabel;
-@property (weak, nonatomic) IBOutlet UILabel *yearLabel;
 
 @end
 
-@implementation DetailsViewController
+@implementation PosterViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -28,38 +25,19 @@
     NSURL *posterURL = [NSURL URLWithString:fullPosterURLString];
     
     [self.posterView setImageWithURL:posterURL];
-    
-    NSString *backdropURLString = self.movie[@"backdrop_path"];
-    NSString *fullBackdropURLString = [baseURLString stringByAppendingString:backdropURLString];
-    NSURL *backdropURL = [NSURL URLWithString:fullBackdropURLString];
-    
-    [self.backdropView setImageWithURL:backdropURL];
-    
     self.titleLabel.text = self.movie[@"title"];
-    self.synopsisLabel.text = self.movie[@"overview"];
-    NSString *year =self.movie[@"release_date"];
-    self.yearLabel.text = [year substringToIndex:4];
-    
     [self.titleLabel sizeToFit];
-    [self.synopsisLabel sizeToFit];
     // Do any additional setup after loading the view.
 }
 
-
+/*
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
-    
-    
-    
-    DetailsViewController *detailsViewController  = [segue destinationViewController];
-    
-    detailsViewController.movie = self.movie;
-    NSLog(@"Tapping on a movie!");
 }
-
+*/
 
 @end
