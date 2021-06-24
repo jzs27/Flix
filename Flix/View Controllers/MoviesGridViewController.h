d29b55d68db506792e151f5f8c95932c1719259a
jzs27 <jzs27@cornell.edu>
2021-06-23 20:59:54 -0500

    collection view setup

---
+++ Flix/View Controllers/MoviesGridViewController.h
@@ -0,0 +1,19 @@
+//
+//  MoviesGridViewController.h
+//  Flix
+//
+//  Created by jessicasyl on 6/23/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MoviesGridViewController : UIViewController
+
+@property (nonatomic, strong) NSArray *movies;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Flix/View Controllers/MoviesGridViewController.m
@@ -0,0 +1,85 @@
+//
+//  MoviesGridViewController.m
+//  Flix
+//
+//  Created by jessicasyl on 6/23/21.
+//
+
+#import "MoviesGridViewController.h"
+#import "MovieCollectionCell.h"
+#import "UIImageView+AFNetworking.h"
+
+@interface MoviesGridViewController () <UICollectionViewDataSource, UICollisionBehaviorDelegate>
+
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+
+@end
+
+@implementation MoviesGridViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.collectionView.dataSource=self;
+    self.collectionView.delegate=self;
+    [self fetchMovies];
+    // Do any additional setup after loading the view.
+}
+
+- (void)fetchMovies{
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+           if (error != nil) {
+               
+               NSLog(@"%@", [error localizedDescription]);
+           }
+           else {
+               
+               NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+               
+               
+               
+               self.movies=dataDictionary[@"results"];
+               [self.collectionView reloadData];
+
+               // TODO: Get the array of movies
+               // TODO: Store the movies in a property to use elsewhere
+               // TODO: Reload your table view data
+           }
+        
+       }];
+    [task resume];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (nonnull __kindof UICollectionViewCell *)collectionView:(nonnull UICollectionView *)collectionView cellForItemAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    MovieCollectionCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"MovieCollectionCell" forIndexPath:indexPath];
+    NSDictionary *movie = self.movies[indexPath.item];
+    NSString *baseURLString = @"https://image.tmdb.org/t/p/w500";
+    NSString *posterURLString = movie[@"poster_path"];
+    NSString *fullPosterURLString = [baseURLString stringByAppendingString:posterURLString];
+    NSURL *posterURL = [NSURL URLWithString:fullPosterURLString];
+    cell.posterView.image=nil;
+    [cell.posterView setImageWithURL:posterURL];
+    
+    return cell;
+}
+
+- (NSInteger)collectionView:(nonnull UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return self.movies.count;
+}
+
+
+
+@end
